fc1ae0f1edbeab58511ddbc904083fe0241d52f8
Kevin Grems <kgrems@epsbuildings.com>
2019-11-06 12:34:57 -0600

    Implemented interface in weapon.

--- Models/Weapon.cs
+++ Models/Weapon.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework.Content;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Mono.Game.Interfaces;
 
@@ -18,9 +19,19 @@ namespace Mono.Game.Models
             this.damage = damage;
         }
 
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            throw new System.NotImplementedException();
+        }
+
         public override void LoadContent(ContentManager contentManager)
         {
             this.Texture = contentManager.Load<Texture2D>(TextureName);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
